05644a72384eb3c6f295aaa168918caf3b6835fc
Emily Deibert <emilydeibert@gmail.com>
2026-06-25 16:22:44 -0400

    fix iter flag

--- src/exopipe/cli/getResults.py
+++ src/exopipe/cli/getResults.py
@@ -110,7 +110,7 @@ def load_and_combine_maps(
 
             if orders is None:
                 #order_sum = np.nansum(fmap, axis=0)
-                order_sum = np.nansum(fmap[cameraDict[goodOrders]], axis=0)
+                order_sum = np.nansum(fmap[cameraDict["goodOrders"]], axis=0)
 
             else:
                 order_sum = np.nansum(fmap[orders], axis=0)
