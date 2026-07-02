6c2b15411fef855f5deee233361aa193d29e85f9
Emily Deibert <emilydeibert@gmail.com>
2026-07-02 10:47:40 +0100

    Update getResults.py

--- src/exopipe/cli/getResults.py
+++ src/exopipe/cli/getResults.py
@@ -542,17 +542,29 @@ def print_gaussian_fit_summary(peak):
     print(
         f"  RV Gaussian fit  : "
         f"{rv_fit['center']:.2f} +/- {rv_fit['center_err']:.2f} km/s"
-        f"fwhm: {rv_fit['fwhm']:.2f}"
-        f"half width: {rv_fit['fit_half_width']:.2f}"
-        f"sigma: {rv_fit['sigma']:.2f}"
+    )
+    print(
+        f"  fwhm  : {rv_fit['fwhm']:.2f}"
+    )
+    print(
+        f"  half width  : {rv_fit['fit_half_width']:.2f}"
+    )
+    print(
+        f"  sigma  : {rv_fit['sigma']:.2f}"
     )
 
     print(
         f"  Kp Gaussian fit  : "
         f"{kp_fit['center']:.2f} +/- {kp_fit['center_err']:.2f} km/s"
-        f"fwhm: {kp_fit['fwhm']:.2f}"
-        f"half width: {kp_fit['fit_half_width']:.2f}"
-        f"sigma: {kp_fit['sigma']:.2f}"
+    )
+    print(
+        f"  fwhm  : {kp_fit['fwhm']:.2f}"
+    )
+    print(
+        f"  half width  : {kp_fit['fit_half_width']:.2f}"
+    )
+    print(
+        f"  sigma  : {kp_fit['sigma']:.2f}"
     )
 
 
